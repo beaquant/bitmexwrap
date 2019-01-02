7689035dfe8d6b485fb3114504b71fabdf01a51f
Btstar <hu.zhihao2002@gmail.com>
2019-01-02 20:06:03 +0800

    add DealAmount

--- order.go
+++ order.go
@@ -3,13 +3,14 @@ package bitmexwrap
 import "time"
 
 type Order struct {
-	OrderID  string
-	Currency string
-	Amount   float64
-	Price    float64
-	PriceAvg float64
-	Status   string
-	Side     string
-	Type     string
-	Time     time.Time
+	OrderID    string
+	Currency   string
+	Amount     float64
+	DealAmount float64
+	Price      float64
+	PriceAvg   float64
+	Status     string
+	Side       string
+	Type       string
+	Time       time.Time
 }
